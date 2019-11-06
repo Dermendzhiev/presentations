7afb7dfd91831fdda158d8dc87eaaa49f489d11f
Nikolay Dermendzhiev <dermendzhiev.n@gmail.com>
2019-11-06 23:23:11 +0200

    Add MatBlazor for material design and Toastr component

--- 2019 Nov - Blazor Sneak Peek/demo/WebAssembly/Blazor.WebAssembly/Startup.cs
+++ 2019 Nov - Blazor Sneak Peek/demo/WebAssembly/Blazor.WebAssembly/Startup.cs
@@ -2,11 +2,20 @@ namespace Blazor.WebAssembly
 {
     using Microsoft.AspNetCore.Components.Builder;
     using Microsoft.Extensions.DependencyInjection;
+using Sotsera.Blazor.Toaster.Core.Models;
 
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add the library to the DI system
+            services.AddToaster(config =>
+            {
+                //example customizations
+                config.PositionClass = Defaults.Classes.Position.TopRight;
+                config.PreventDuplicates = true;
+                config.NewestOnTop = false;
+            });
         }
 
         public void Configure(IComponentsApplicationBuilder app) => app.AddComponent<App>("app");
